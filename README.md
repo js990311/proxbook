6f53bf037522d0ca597a94be57edf3d8e5be5060
Lee jong sik <102013392+js990311@users.noreply.github.com>
2024-05-18 23:02:17 +0900

    [docker] librarySearchService
    개발 및 테스트를 위한 docker 추가
    librarySearchService test 추가

---
+++ README.md
@@ -0,0 +1,10 @@
+# Proxbook finder service
+
+proxbook은 proximity와 book의 합성어입니다.   
+이 서비스는 내 주변의 도서관과 내 주변 도서관의 소장도서를 서비스합니다. 
+
+## 데이터 출처 
+
+- [문화빅데이터포털 - 공공 도서관 소장도서](https://www.bigdata-culture.kr/bigdata/user/data_market/detail.do?id=9bc56c9c-bc4e-4b68-90da-f4900009fc05)
+- [문화빅데이터포털 - 공공 도서관 정보](https://www.bigdata-culture.kr/bigdata/user/data_market/detail.do?id=7461f23e-8958-417b-be03-7ede86ab760b)
+
---
+++ src/test/java/com/proxbook/finder/domain/library/service/LibrarySearchServiceTest.java
@@ -0,0 +1,38 @@
+package com.proxbook.finder.domain.library.service;
+
+import com.proxbook.finder.domain.library.dto.LibraryDto;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class LibrarySearchServiceTest {
+
+    @Autowired private LibrarySearchService librarySearchService;
+
+    @ParameterizedTest
+    @MethodSource("provideCityNameAndCount")
+    void readLibraryByLibraryName(String cityname, int count) {
+        List<LibraryDto> libraries = librarySearchService.readLibraryByLibraryName(cityname);
+        assertEquals(count, libraries.size());
+    }
+
+    static Stream<Arguments> provideCityNameAndCount(){
+        return Stream.of(
+                Arguments.of("서울", 23),
+                Arguments.of("부산", 49),
+                Arguments.of("대구", 22),
+                Arguments.of("제주", 13)
+        );
+    }
+}
