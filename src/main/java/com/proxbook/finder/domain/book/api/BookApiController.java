f552b484ab75884a8593f05d1f104de2482a6ead
Lee jong sik <102013392+js990311@users.noreply.github.com>
2024-08-26 21:25:03 +0900

    add api-prox

--- src/main/java/com/proxbook/finder/domain/book/api/BookApiController.java
+++ src/main/java/com/proxbook/finder/domain/book/api/BookApiController.java
@@ -1,9 +1,12 @@
 package com.proxbook.finder.domain.book.api;
 
+import com.proxbook.finder.application.form.LibraryForm;
 import com.proxbook.finder.domain.book.dto.BookDto;
 import com.proxbook.finder.domain.book.service.BookService;
 import com.proxbook.finder.domain.library.dto.LibraryDto;
 import com.proxbook.finder.domain.library.service.LibraryService;
+import com.proxbook.finder.domain.proxlibrary.dto.UserProxLibraryDto;
+import com.proxbook.finder.domain.proxlibrary.service.UserProxLibraryService;
 import com.proxbook.finder.global.response.BaseListResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ui.Model;
@@ -17,6 +20,7 @@ import java.util.List;
 public class BookApiController {
     private final BookService bookService;
     private final LibraryService libraryService;
+    private final UserProxLibraryService userProxLibraryService;
 
     @GetMapping()
     public BaseListResponse<BookDto> searchBookByName(@RequestParam(value = "title", required = false) String title){
@@ -24,12 +28,17 @@ public class BookApiController {
     }
 
     @GetMapping("/{id}")
-    public BookDto getBookByBookId(@PathVariable("id") Long id, Model model){
+    public BookDto getBookByBookId(@PathVariable("id") Long id){
         return bookService.readBookByBookId(id);
     }
 
     @GetMapping("/{id}/library")
-    public BaseListResponse<LibraryDto> getLibraryByBookId(@PathVariable("id") Long id, Model model){
+    public BaseListResponse<LibraryDto> getLibraryByBookId(@PathVariable("id") Long id){
         return new BaseListResponse<>(libraryService.readLibraryByBookId(id));
+    }
+
+    @PostMapping("/{id}/prox")
+    public UserProxLibraryDto getProxLibraryBook(@PathVariable("id") Long id, @RequestBody LibraryForm form){
+        return userProxLibraryService.createUserProxLibraryByBookIdAndGeo(id, form.getLatitude(), form.getLongitude(), 10.0);
     }
 }
--- src/main/java/com/proxbook/finder/domain/library/api/LibraryApiController.java
+++ src/main/java/com/proxbook/finder/domain/library/api/LibraryApiController.java
@@ -4,17 +4,18 @@ import com.proxbook.finder.application.form.LibraryForm;
 import com.proxbook.finder.domain.library.dto.LibraryBookDto;
 import com.proxbook.finder.domain.library.dto.LibraryDto;
 import com.proxbook.finder.domain.library.service.LibraryService;
+import com.proxbook.finder.domain.proxlibrary.dto.UserProxLibraryDto;
+import com.proxbook.finder.domain.proxlibrary.service.UserProxLibraryService;
 import com.proxbook.finder.global.response.BaseListResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RequiredArgsConstructor
 @RequestMapping("/api/library")
 @RestController
-public class LibraryRestController {
+public class LibraryApiController {
     private final LibraryService libraryService;
+    private final UserProxLibraryService userProxLibraryService;
 
     @GetMapping
     public BaseListResponse<LibraryDto> search(@RequestParam("name") String name){
@@ -30,4 +31,10 @@ public class LibraryRestController {
     public LibraryBookDto findLibraryBookByLibraryId(@RequestParam("id") Long id){
          return libraryService.readLibraryBooksByLibraryId(id);
     }
+
+    @PostMapping("/prox")
+    public UserProxLibraryDto getProxLibraryBook(@RequestBody LibraryForm form){
+        return userProxLibraryService.createUserProxLibraryByGeo(form.getLatitude(), form.getLongitude(), 10.0);
+    }
+
 }
